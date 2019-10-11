c1954dd18e091cd472e8d11b9a8b4ad393ff11fe
Ben Lewis <blewis@theparkingspot.com>
2019-10-11 15:37:42 -0500

    add api keys

---
+++ Virtual Tourist/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  Virtual Tourist
+//
+//  Created by Ben Lewis on 10/11/19.
+//  Copyright © 2019 Ben Lewis. All rights reserved.
+//
+
+import Foundation
+
+enum Secrets : String {
+    case apiKey = "INSERT_API_KEY_HERE"
+    case secret = "INSERT_SECRET_KEY_HERE"
+}
